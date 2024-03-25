be110c7aa7e4e3361b3cb1889f4bcbf624f5d6ee
NarutoUchiha39 <anirudhata123@gmail.com>
2024-03-25 22:00:45 +0530

    Added Cors

---
+++ MusicService/src/main/java/com/Spring_IA_2/PomodoroWebsite/configuration/CorsConfigurations.java
@@ -0,0 +1,20 @@
+package com.Spring_IA_2.PomodoroWebsite.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurations {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/api/getSongsList").allowedOrigins("*");
+            }
+        };
+    }
+}
