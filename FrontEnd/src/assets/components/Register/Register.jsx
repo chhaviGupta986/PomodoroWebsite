cd4be5f9ba58f2ce3ea406fa76d4079cf0ca4ea0
NarutoUchiha39 <anirudhata123@gmail.com>
2024-04-06 11:00:22 +0530

    Completed basic register yet to populate user as a global prop

--- FrontEnd/src/assets/components/Register/Register.jsx
+++ FrontEnd/src/assets/components/Register/Register.jsx
@@ -1,42 +1,71 @@
 import React from 'react'
 import '../Login/Login.css'
-function Register() {
+function Register(props) {
+	function submitHandler(event){
+		event.preventDefault()
+		const userName = event.target.user.value
+		const pass = event.target.pass.value
+		const mobileNumber = event.target.mobile.value
+		const Email = event.target.email.value
+
+		let send =JSON.stringify({
+			username: userName,
+			password: pass,
+			email: Email,
+			roles: [{role_name:"USER"}],
+			mobile: mobileNumber
+		  }
+		)
+
+		console.log(send)
+		async function sendData(){
+		
+		
+			const data = await fetch("http://localhost:8080/register",{
+				method:"POST",
+				headers:{"Content-Type":"application/json"},
+				body:send
+			}).then(async(res)=>{return await res.json()})
+			console.log(data)
+		}
+		sendData()
+	}
   return (
     <>
-    <div class="login-wrap">
-	<div class="login-html">
-		<input id="tab-1" type="radio" name="tab" class="sign-in" checked/><label for="tab-1" class="tab"></label>
-		<input id="tab-2" type="radio" name="tab" class="sign-up"/><label for="tab-2" class="tab">Sign Up</label>
-		<div class="login-form">
+    <form className="login-wrap" onSubmit={submitHandler}>
+	<div className="login-html">
+		<input id="tab-1" type="radio" name="tab" className="sign-in" checked/><label htmlFor="tab-1" className="tab"></label>
+		<input id="tab-2" type="radio" name="tab" className="sign-up"/><label htmlFor="tab-2" className="tab">Sign Up</label>
+		<div className="login-form">
 			
-			<div class="sign-up-htm">
-				<div class="group">
-					<label for="user" class="label">Username</label>
-					<input id="user" type="text" class="input"/>
+			<div className="sign-up-htm">
+				<div className="group">
+					<label htmlFor="user" className="label">Username</label>
+					<input id="user" type="text" className="input"/>
 				</div>
-				<div class="group">
-					<label for="pass" class="label">Password</label>
-					<input id="pass" type="password" class="input" data-type="password"/>
+				<div className="group">
+					<label htmlFor="pass" className="label">Password</label>
+					<input id="pass" type="password" className="input" data-type="password"/>
 				</div>
-				<div class="group">
-					<label for="pass" class="label">Repeat Password</label>
-					<input id="pass" type="password" class="input" data-type="password"/>
+				<div className="group">
+					<label htmlFor="mobile" className="label">Mobile Number</label>
+					<input id="mobile" type="number" className="input"/>
 				</div>
-				<div class="group">
-					<label for="pass" class="label">Email Address</label>
-					<input id="pass" type="text" class="input"/>
+				<div className="group">
+					<label htmlFor="email" className="label">Email Address</label>
+					<input id="email" type="email" className="input"/>
 				</div>
-				<div class="group">
-					<input type="submit" class="button" value="Sign Up"/>
+				<div className="group">
+					<input type="submit" className="button" value="Sign Up"/>
 				</div>
-				<div class="hr"></div>
-				<div class="foot-lnk">
-          <label for="tab-1">Already Member</label>
+				<div className="hr"></div>
+				<div className="foot-lnk">
+          <label htmlFor="tab-1">Already Member</label>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</form>
                  
 
       </>
