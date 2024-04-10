70aa1117e230901afbbe3b4f716e9413b849b7e0
NarutoUchiha39 <anirudhata123@gmail.com>
2024-04-11 00:08:40 +0530

    Added skeleton of TrackUserMicroService

---
+++ TrackUser/src/main/java/com/UserMicroService/TrackUser/TrackUserApplication.java
@@ -0,0 +1,13 @@
+package com.UserMicroService.TrackUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackUserApplication.class, args);
+	}
+
+}
